fix sign error

tests/test_hist2d.py
@@ -65,10 +65,30 @@ def test_profile():
     xs = np.array([0.5, 1.5])
     ys = np.array([1.5, 0.5])
     bins = np.array([0, 1, 2])
-    h1 = Hist2D(np.c_[xs, ys], bins=bins)
+    h1 = Hist2D((xs, ys), bins=bins)
+
     assert h1.profile("x") == h1.profile("y")
     allclose(h1.profile("x").counts, np.array([1.5, 0.5]))
 
+    assert h1.profile(0) == h1.profile(1)
+
+def test_profile_against_root():
+    # import ROOT
+    # h = ROOT.TH2F("h","", 5,-5,5, 5,-5,5)
+    # for x,y in xy:
+    #     h.Fill(x,y)
+    # hp = h.ProfileX()
+    # for i in [2,3,4]:
+    #     print(hp.GetBinContent(i), hp.GetBinError(i)**2)
+    xy = np.array([
+        [-2, 1.5], [-2, -3.5], [-2, 1.5], # x = -2
+        [0., -2.], [0., -2.], [0., 0.], [0., 2.], [0., 4.], # x = 0
+        [2, 1.5], # x = +2
+        ])
+    h = Hist2D(xy, bins="5,-5,5", overflow=False)
+    hp = h.profile("x")
+    allclose(hp.counts[1:-1], [0.0, 0.4, 2.0])
+    allclose(hp.errors[1:-1]**2, [2.66666667, 1.088, 0.0])
 
 def test_projection():
     xs = np.array([0.5, 1.5])

yahist/hist2d.py
@@ -237,7 +237,7 @@ class Hist2D(Hist1D):
         num_err = np.matmul(errors.T ** 2, centers ** 2) ** 0.5
         den_err = np.sum(errors ** 2, axis=0) ** 0.5
         r_val = num / den
-        r_err = ((num_err / den) ** 2 + (den_err * num / den ** 2.0) ** 2.0) ** 0.5
+        r_err = ((num_err / den) ** 2 - (den_err * num / den ** 2.0) ** 2.0) ** 0.5
         hnew = Hist1D()
         hnew._counts = r_val
         hnew._errors = r_err